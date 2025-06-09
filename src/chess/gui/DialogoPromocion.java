4df1c38a5564561450ee5e9dd5200c3bff773c97
Mr-0bo <mrobo.idk@gmail.com>
2025-06-09 00:08:08 -0600

    Update DialogoPromocion.java

--- src/chess/gui/DialogoPromocion.java
+++ src/chess/gui/DialogoPromocion.java
@@ -8,18 +8,23 @@ import chess.piezas.Reina;
 import chess.piezas.Torre;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
 public class DialogoPromocion {
     public static Pieza mostrarDialogoPromocion(Color color) {
+        ImageIcon iconoOriginal = new ImageIcon("src/resources/promocion.png");
+        Image imagen = iconoOriginal.getImage();
+        Image imagenNueva = imagen.getScaledInstance(154, 102, Image.SCALE_SMOOTH);
+        ImageIcon icono = new ImageIcon(imagenNueva);
         List<String> opciones = Arrays.asList("Reina", "Torre", "Alfil", "Caballo");
         String eleccion = (String) JOptionPane.showInputDialog(
                 null,
                 "Elige una pieza:",
                 "Promoción",
                 JOptionPane.QUESTION_MESSAGE,
-                null,
+                icono,
                 opciones.toArray(),
                 "Reina");
         if (eleccion == null) {
