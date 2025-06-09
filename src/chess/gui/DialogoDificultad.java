d4d82615f7f15831e8c66003f8ad82bfd9dacf44
Mr-0bo <mrobo.idk@gmail.com>
2025-06-09 00:08:31 -0600

    Update DialogoDificultad.java

--- src/chess/gui/DialogoDificultad.java
+++ src/chess/gui/DialogoDificultad.java
@@ -1,22 +1,37 @@
 package chess.gui;
 
 import javax.swing.*;
+import java.awt.Image;
 
 public class DialogoDificultad {
     public static int mostrarDialogoDificultad() {
-        Object[] opciones = {"Principiante (500 ms)", "Intermedio (1000 ms)", "Experto (2000 ms)", "Magnus Carlsen (2882 ms)"};
+        Object[] opciones = {
+                "Principiante (500 ms)",
+                "Intermedio (1000 ms)",
+                "Experto (2000 ms)",
+                "Magnus Carlsen (2882 ms)"
+        };
+
+        // Imagen
+        ImageIcon iconoOriginal = new ImageIcon("src/resources/dificultad.png");
+        Image imagen = iconoOriginal.getImage();
+        Image imagenNueva = imagen.getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+        ImageIcon icono = new ImageIcon(imagenNueva);
+
         String eleccion = (String) JOptionPane.showInputDialog(
                 null,
                 "Elige la dificultad:",
                 "Dificultad",
                 JOptionPane.QUESTION_MESSAGE,
-                null,
+                icono,
                 opciones,
-                "Intermedio (1000 ms)");
+                "Intermedio (1000 ms)"
+        );
+
         if (eleccion == null) {
             eleccion = "Intermedio (1000 ms)";
         }
-        switch(eleccion) {
+        switch (eleccion) {
             case "Principiante (500 ms)":
                 return 500;
             case "Intermedio (1000 ms)":
@@ -29,4 +44,5 @@ public class DialogoDificultad {
                 return 1000;
         }
     }
+}
 }
